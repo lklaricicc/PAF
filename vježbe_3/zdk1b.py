ae057ef395d3458c35895b08d854913f169b5eba
lklaricicc <lklaricic@pmfst.hr>
2024-03-22 10:58:10 +0100

    commit

--- vježbe_3/zdk1b.py
+++ vježbe_3/zdk1b.py
@@ -1,4 +1,8 @@
 rezultat = 0.1+0.2+0.3
-print(rezultat == 0.6) 
+if rezultat == 0.6:
+    print("True")
+else:
+    print("False")
+print(rezultat) 
 # dobijem da je rezultat False, vrlo vjerojatno zato što 
 #ne koristim naredbu float pa sustav drugačije pohranjuje decimalne brojeve
