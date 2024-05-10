997cdbe5d326a188cf5e9d9ba4e942dfc179d8f8
lklaricicc <lklaricic@pmfst.hr>
2024-05-10 11:58:11 +0200

    commit

--- vježbe_8/program.py
+++ vježbe_8/program.py
@@ -1,17 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-class čestica: #definiram klasu čestica koja modelira svojstva i ponašanja čestice čije gibanje opisujem
+class čestica: #definiram klasu čestica koja modelira vrijednosti i ponašanja čestice čije gibanje opisujem
     def __init__(self,m,q,p,v,E,B,dt):#def __init__ definira se kao konstruktor klase čestica, on inicijalizira objekt sa zadanim parametrima
         #zadani parametri: masa m, naboj q, pozicija p, brzina v, električno polje E, magnetsko polje B, vrijeme izraženo kao dt
         self.m = m
         self.q = q 
-        #self.m = m i self.q = q su atributi objekta na predane vrijednosti
+        #self.m = m i self.q = q su atributi objekta na zadane vrijednosti, u prijveodu postavljam m ili q unutar tog objekta na neku vrijednost m ili q koju sam definirala
         self.p = np.array(p)
         self.v = np.array(v)
         self.E = np.array (E)
         self.B = np.array (B)
         self.dt = dt
+        #ove linije koda omogućuju "čestici" da zna koje su njegove karakteristike 
     
     def gibanje (self, t): #definiram metodu gibanje koja simulira gibanje čestice u vremenskom intervalu t
         t0 = 0 #vrijeme namještam na nulu
