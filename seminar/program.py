fc6d59d5e60d4aca62ed421394159aaeb83ef4f8
lklaricicc <lklaricic@pmfst.hr>
2024-05-24 17:46:48 +0200

    seminar

---
+++ seminar/program.py
@@ -0,0 +1,46 @@
+#dio iz vježbe 8
+import numpy as np
+
+class čestica:
+    def __init__(self, m, q, p, v, E, B, dt):
+        self.m = m
+        self.q = q
+        self.p = np.array(p, dtype=float)
+        self.v = np.array(v, dtype=float)
+        self.E = np.array(E, dtype=float)
+        self.B = np.array(B, dtype=float)
+        self.dt = dt
+
+    def gibanje_euler(self, t):
+        t0 = 0
+        putanja = []
+        vrijeme = np.arange(t0, t, self.dt)
+        for _ in vrijeme:
+            a = (self.q / self.m) * (self.E + np.cross(self.v, self.B))
+            self.v = self.v + a * self.dt
+            self.p = self.p + self.v * self.dt
+            putanja.append(self.p.copy())
+        return np.array(putanja)
+    
+    def gibanje_rk4(self, t):
+        t0 = 0
+        putanja = []
+        vrijeme = np.arange(t0, t, self.dt)
+        for _ in vrijeme:
+            p1, v1 = self.p, self.v
+            a1 = (self.q / self.m) * (self.E + np.cross(v1, self.B))
+            
+            p2, v2 = self.p + 0.5 * self.dt * v1, self.v + 0.5 * self.dt * a1
+            a2 = (self.q / self.m) * (self.E + np.cross(v2, self.B))
+            
+            p3, v3 = self.p + 0.5 * self.dt * v2, self.v + 0.5 * self.dt * a2
+            a3 = (self.q / self.m) * (self.E + np.cross(v3, self.B))
+            
+            p4, v4 = self.p + self.dt * v3, self.v + self.dt * a3
+            a4 = (self.q / self.m) * (self.E + np.cross(v4, self.B))
+            
+            self.p = self.p + (self.dt / 6.0) * (v1 + 2 * v2 + 2 * v3 + v4)
+            self.v = self.v + (self.dt / 6.0) * (a1 + 2 * a2 + 2 * a3 + a4)
+            
+            putanja.append(self.p.copy())
+        return np.array(putanja)
---
+++ seminar/zadatak.py
@@ -0,0 +1,65 @@
+#oba koda u jednom ali s odvojenim grafovima na finalnom prikazu
+
+import matplotlib.pyplot as plt
+import numpy as np
+import program as pg
+
+# Postavke čestica
+masa = 0.3
+naboj_elektron = -1
+naboj_pozitron = 1
+pozicija = (0, 0, 0)
+brzina = (0.1, 0.1, 0.1)
+elektricno_polje = (0, 0, 0)
+magnetsko_polje = (0, 0, 1)
+dt = 0.01
+t = 5
+
+# Kreiranje objekata čestica
+elektron_euler = pg.čestica(masa, naboj_elektron, pozicija, brzina, elektricno_polje, magnetsko_polje, dt)
+pozitron_euler = pg.čestica(masa, naboj_pozitron, pozicija, brzina, elektricno_polje, magnetsko_polje, dt)
+
+# Izračun putanja korištenjem Eulerove metode
+putanja_euler_elektron = elektron_euler.gibanje_euler(t)
+putanja_euler_pozitron = pozitron_euler.gibanje_euler(t)
+
+# Resetiranje početnih uvjeta za Runge-Kutta metodu
+elektron_rk4 = pg.čestica(masa, naboj_elektron, pozicija, brzina, elektricno_polje, magnetsko_polje, dt)
+pozitron_rk4 = pg.čestica(masa, naboj_pozitron, pozicija, brzina, elektricno_polje, magnetsko_polje, dt)
+
+# Izračun putanja korištenjem Runge-Kutta metode
+putanja_rk4_elektron = elektron_rk4.gibanje_rk4(t)
+putanja_rk4_pozitron = pozitron_rk4.gibanje_rk4(t)
+
+# Prikazivanje rezultata u odvojenim grafovima
+fig = plt.figure(figsize=(12, 10))
+
+# Graf za Eulerovu metodu - Elektron
+ax1 = fig.add_subplot(221, projection='3d')
+ax1.plot(putanja_euler_elektron[:, 0], putanja_euler_elektron[:, 1], putanja_euler_elektron[:, 2], color = 'blue', label='Elektron (Euler)')
+ax1.set_title('Elektron - Eulerova metoda')
+ax1.legend()
+
+# Graf za Eulerovu metodu - Pozitron
+ax2 = fig.add_subplot(222, projection='3d')
+ax2.plot(putanja_euler_pozitron[:, 0], putanja_euler_pozitron[:, 1], putanja_euler_pozitron[:, 2], color = 'orange',label='Pozitron (Euler)')
+ax2.set_title('Pozitron - Eulerova metoda')
+ax2.legend()
+
+# Graf za Runge-Kutta metodu - Elektron
+ax3 = fig.add_subplot(223, projection='3d')
+ax3.plot(putanja_rk4_elektron[:, 0], putanja_rk4_elektron[:, 1], putanja_rk4_elektron[:, 2], color = 'green', label='Elektron (RK4)')
+ax3.set_title('Elektron - Runge-Kutta metoda')
+ax3.legend()
+
+# Graf za Runge-Kutta metodu - Pozitron
+ax4 = fig.add_subplot(224, projection='3d')
+ax4.plot(putanja_rk4_pozitron[:, 0], putanja_rk4_pozitron[:, 1], putanja_rk4_pozitron[:, 2], color = 'red', label='Pozitron (RK4)')
+ax4.set_title('Pozitron - Runge-Kutta metoda')
+ax4.legend()
+
+plt.subplots_adjust (hspace = 0.5)
+
+
+plt.tight_layout()
+plt.show()
